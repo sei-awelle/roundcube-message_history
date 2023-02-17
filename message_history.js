1ee1b4ff3ebb52ea6685bc08b88580199f53be75
Nuria Pacheco <npacheco@cert.org>
2023-02-17 16:59:15 -0500

    Added search bar functionality on the table as well as the filter functionality by cliking on the headers. Added fucntionality to make this plugin available only to approved users. Made the logs logo persistent.

---
+++ message_history.js
@@ -0,0 +1,112 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const headerTitle = document.querySelector('.header-title');
+  headerTitle.innerHTML = 'Roundcube Logs';
+	headerTitle.classList.add('plugin-title');
+  const table = document.getElementById('message_history_v2');
+  const headers = table.getElementsByTagName('th');
+  
+  for (let i = 0; i < headers.length; i++) {
+    headers[i].innerHTML += '<span class="arrow"></span>';
+    headers[i].setAttribute('onclick', 'sortTable(' + i + ')');
+  }
+
+  // Create a new div for the search bar
+  const searchBar = document.createElement('div');
+  searchBar.style.marginBottom = '10px';
+
+  // Create an input element for the search box
+  const searchBox = document.createElement('input');
+  searchBox.type = 'text';
+  searchBox.placeholder = 'Search';
+  searchBox.style.marginRight = '10px';
+
+  // Add an event listener to the search box to filter the table
+  searchBox.addEventListener('input', function() {
+    const query = searchBox.value.toLowerCase();
+    const rows = table.querySelectorAll('tbody tr');
+  
+    rows.forEach(row => {
+      const cells = row.querySelectorAll('td');
+      let shouldHide = true;
+  
+      cells.forEach(cell => {
+        if (cell.textContent.toLowerCase().indexOf(query) > -1) {
+          shouldHide = false;
+        }
+      });
+  
+      if (shouldHide) {
+        row.style.display = 'none';
+      } else {
+        row.style.display = '';
+      }
+    });
+  });
+
+  // Add the search box to the search bar div
+  searchBar.appendChild(searchBox);
+
+  // Add the search bar div to the table container
+  table.parentNode.insertBefore(searchBar, table);
+
+ $('#message_history_v2').css('overflow-y', 'scroll');
+});
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("message_history_v2");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("td")[n];
+      y = rows[i + 1].getElementsByTagName("td")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+  // Toggle the direction of the arrow:
+  var arrow = table.getElementsByTagName('th')[n].querySelector('.arrow');
+  arrow.classList.toggle('arrow-up');
+  arrow.classList.toggle('arrow-down');
+}
--- message_history.php
+++ message_history.php
@@ -9,18 +9,16 @@ class message_history extends rcube_plugin
 		$rcmail = rcmail::get_instance();
 		$this->rc = &$rcmail;
 		$this->add_texts('localization/', true);
-		$this->add_hook('message_ready', array($this, 'log_message'));
 		$this->load_config();
-
+		$user = $rcmail->user->get_username();
+		preg_match('/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/', $user, $matches);
+		$email = $matches[0];	
+		$db = rcmail::get_instance()->get_dbh();
+		$result = $db->query("SELECT * FROM message_history_users WHERE email = '$email'");
 		// get admin user from config
 		// if this is the admin user
-		if (true) {
-			// add button and hook for the settings page that allows the admin user to select users that can view the logs page
-		}
-
-		// get user perms
 		// if user has perms
-		if (true) {
+		if ($result->rowCount() > 0) {
 
 			$this->register_action('plugin.message_history', array($this, 'message_history_init'));
 			//$this->register_action('message_history.message_history', array($this, 'message_history_init'));
@@ -29,8 +27,10 @@ class message_history extends rcube_plugin
 			//$this->add_hook('template_container', array($this, 'add_button_to_sidebar'));
 
 			$this->add_hook('message_ready', array($this, 'log_message'));
-
+			
 			$this->register_task('message_history');
+			$skin_path = $this->local_skin_path();
+			$this->include_stylesheet("$skin_path/message_history.css");
 			$this->add_button(
 			[
 				'type' => 'link',
@@ -74,9 +74,9 @@ class message_history extends rcube_plugin
 	public function logs_form()
 	{
 		$rcmail = rcmail::get_instance();
-		$user = $rcmail->user;
-		$identity = $user->get_identity();
-
+		//$user = $rcmail->user;
+		//$identity = $user->get_identity();
+		$this->include_script('message_history.js');
 		$skin_path = $this->local_skin_path();
 		$this->include_stylesheet("$skin_path/message_history.css");
 	
@@ -95,7 +95,7 @@ class message_history extends rcube_plugin
 		//$result = $db->query("SELECT * FROM message_history");
 		$result = $db->query("SELECT * FROM message_history_v2");
 		$records = $result->fetchAll();
-		
+				
 		$table = new html_table(['id' => 'message_history_v2', 'class' => 'message_history_table']);
 		$table->add_header('table_header','From');
 		$table->add_header('table_header','To');
@@ -117,10 +117,11 @@ class message_history extends rcube_plugin
 		//$output .= html::span('logs', 'hello world');
 		//$output .= $table->show();
 		//$output .= html::br();
-		$output = html::p('h2', "Roundcube Logs");
-		$output .= html::br();
+		//$output = html::header('title', "Roundcube Logs");
+		
+		//$output = html::p('h1', 'Roundcube Logs');
+		$output = html::br();
 		$output .= html::p('p', "This table will provide analytical information of emails being sent by providing information such as the sender, recipient, subject of the email, and the time it was sent.");
-		$output .= html::br();
 		$output .= $table->show();
 		//$attrib = ['id' => 'message_history'];
 		//$attrib = ['id' => 'message_history_v2', 'class' => 'message_history_table' ];
@@ -162,16 +163,16 @@ class message_history extends rcube_plugin
 		$records = $db->fetch_assoc($result);
 		//$from_id = $records['contact_id'];
 		$from_name = $records['email'];
-
+		
 		//$to_id = "20";
-		$to_name = "20";
-
+		//$to_name = "20";
+		
 		//$table = 'message_history';
 		$table = 'message_history_v2';
 		//$result = $db->query("INSERT INTO $table (from_user_id, to_user_id, subject, time_sent, modified) VALUES (?, ?, ?, ?, ?)",
 		//$from_id, $to_id, $subject, $date, $db->now());
 		$result = $db->query("INSERT INTO $table (from_user_name, to_user_name, subject, time_sent, modified) VALUES (?, ?, ?, ?, ?)",
-			$from_name, $to_name, $subject, $date, $db->now());
+			$from_name, $to, $subject, $date, $db->now());
 		if ($db->is_error($result))
 		{
 			rcube::raise_error([
