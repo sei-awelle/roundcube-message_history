112afe82694b16b897d6f5ee4d74b0125854cd05
Adam R. Welle <arwelle@cert.org>
2023-06-14 17:17:03 -0400

    updates SQL/postgresql.sql

--- SQL/postgresql.sql
+++ SQL/postgresql.sql
@@ -1,10 +1,12 @@
-CREATE TABLE IF NOT EXISTS "message_history" (
+CREATE TABLE IF NOT EXISTS "message_history_v2” (
 	"message_id" SERIAL PRIMARY KEY,
-        "from_user_id" int NOT NULL,
-	"to_user_id" int NOT NULL,
-	"subject" varchar(255) NOT NULL,
+	"from_user_name" varchar(255) NOT NULL,
+	"to_user_name" varchar(255) NOT NULL,
+	"subject" varchar(255) NOT NULL
 	"time_sent" timestamptz NOT NULL,
-	"modified" timestamptz NOT NULL
+	"modified" timestamptz NOT NULL,
+	"read_status" BOOLEAN DEFAULT false,
+	"roundcube_message_id" varchar(255),
+	"exercise" varchar(255)
 );
-
 
